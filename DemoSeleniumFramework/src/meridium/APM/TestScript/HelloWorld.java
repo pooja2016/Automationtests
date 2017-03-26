efe87e00631d9a03b32c8be3ca3336412b270c62
pooja2016 <poojasharma.dli@gmail.com>
2017-03-26 07:41:11 +0530

    updated java code

--- DemoSeleniumFramework/src/meridium/APM/TestScript/HelloWorld.java
+++ DemoSeleniumFramework/src/meridium/APM/TestScript/HelloWorld.java
@@ -1,9 +1,10 @@
-Public class HelloWorld{
-public static void main STring args[]{
+package meridium.APM.TestScript;
 
+public class HelloWorld{
+public static void main (String args[])
+{
 System.out.println("Hello Pooja to Selenium world");
 System.out.println("Hello Pooja to Github world");
 System.out.println("Hello Pooja to Jenkins world");
-
 }
 }
