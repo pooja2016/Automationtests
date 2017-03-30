6c1c899e3fba0fe636362996d93979fddeda8dbe
pooja2016 <poojasharma.dli@gmail.com>
2017-03-30 14:01:02 +0530

    Update HelloWorld.java

--- DemoSeleniumFramework/src/meridium/APM/TestScript/HelloWorld.java
+++ DemoSeleniumFramework/src/meridium/APM/TestScript/HelloWorld.java
@@ -1,10 +1,11 @@
 package meridium.APM.TestScript;
 import org.testng.annotations.Test;
-import org.openqa.selenium.*;
 
-@Test
 public class HelloWorld{
 //public static void main (String args[])
+
+@Test
+public void helloMethod()
 {
 System.out.println("Hello Pooja to Selenium world");
 System.out.println("Hello Pooja to Github world");
